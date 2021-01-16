27cfaefb2fa1df864eadbd5e2a1530544ec47803
Mukul Agarwal <55563106+agarmu@users.noreply.github.com>
2021-01-16 12:35:09 -0600

    add validation + tests to Production Rules

--- Sources/Lindenmayer/ProductionRule.swift
+++ Sources/Lindenmayer/ProductionRule.swift
@@ -4,15 +4,18 @@
 //
 //  Created by Mukul Agarwal on 1/15/21.
 //
-
 import Foundation
 
-public class ProductionRule<T, U: Collection> : CustomStringConvertible
+public class ProductionRule<T: Hashable, U: Collection> : CustomStringConvertible
     where U.Element == T
 {
     let predecessor: T;
     let successor: U;
-
+    
+    enum ProductionRuleError: Error {
+        case validationError(_ invalid: T, predecessor: Bool, _ alphabet: Set<T>)
+    }
+    
     //impl: CustomStringConvertible
     public var description: String {
         "\(predecessor) -> \(successor)"
@@ -22,16 +25,20 @@ public class ProductionRule<T, U: Collection> : CustomStringConvertible
         self.predecessor = predecessor
         self.successor = successor
     }
+    convenience init( _ p: T, _ s:U) {
+        self.init(predecessor: p, successor: s)
+    }
 
 }
 extension ProductionRule where T: Equatable{
-    func validate(_ validator: (T) -> Bool) -> Bool {
-        guard validator(self.predecessor) else {
-            return false
+    func validate(_ alphabet: Set<T>) throws {
+        guard alphabet.contains(self.predecessor) else {
+            throw ProductionRuleError.validationError(predecessor, predecessor: true, alphabet)
         }
-        /*for item in self.successor {
-            
-        }*/
-        return true
+        for item in self.successor {
+            if !alphabet.contains(item) {
+                throw ProductionRuleError.validationError(item, predecessor: false, alphabet)
+            }
+        }
     }
 }
---
+++ Tests/LindenmayerTests/ProductionRuleTests.swift
@@ -0,0 +1,70 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mukul Agarwal on 1/16/21.
+//
+
+import XCTest
+@testable import Lindenmayer
+
+final class ProductionRuleTests: XCTestCase {
+    typealias StringRule = ProductionRule<Character, String>
+    typealias EnumRule = ProductionRule<LState, [LState]>
+    enum LState: Equatable {
+        case F
+        case B
+        case L
+        case R
+    }
+    
+    func testProductionRuleCreationString() {
+        let rule: StringRule = .init("A", "AB")
+        XCTAssertEqual(rule.predecessor, "A")
+        XCTAssertEqual(rule.successor, "AB")
+    }
+    func testProductionRuleCreationEnum() {
+        let rule: EnumRule = .init(.F, [.B, .L, .R])
+        XCTAssertEqual(rule.predecessor, .F)
+        XCTAssertEqual(rule.successor, [.B, .L, .R])
+    }
+    func testProductionRuleValidationStringPredecessorError() {
+        let rule: StringRule = .init("A", "BCD")
+        let predecessorBadAlphabet: Set<Character> = Set(arrayLiteral: "B", "C")
+        XCTAssertThrowsError(try rule.validate(predecessorBadAlphabet)) { error in
+            let error = error as! StringRule.ProductionRuleError
+            switch error {
+            case .validationError(let invalid, let predecessor, let alphabet):
+                XCTAssertEqual(invalid, "A")
+                XCTAssertEqual(predecessor, true)
+                XCTAssertEqual(alphabet, predecessorBadAlphabet)
+            default:
+                XCTFail("not a validationError")
+                
+            }
+        }
+    }
+    func testProductionRuleValidationStringSuccessorError() {
+        let rule: StringRule = .init("A", "BCD")
+        let successorBadAlphabet: Set<Character> = Set(arrayLiteral: "A", "B", "C")
+        XCTAssertThrowsError(try rule.validate(successorBadAlphabet)) { error in
+            let error = error as! StringRule.ProductionRuleError
+            switch error {
+            case .validationError(let invalid, let predecessor, let alphabet):
+                XCTAssertEqual(invalid, "D")
+                XCTAssertEqual(predecessor, false)
+                XCTAssertEqual(alphabet, successorBadAlphabet)
+            default:
+                XCTFail("not a validationError")
+                
+            }
+        }
+
+    }
+    func testProductionRuleValidationStringShouldPass() {
+        let rule: StringRule = .init("A", "BCD")
+        let goodAlphabet: Set<Character> = Set(arrayLiteral: "A", "B", "C", "D")
+        XCTAssertNoThrow(try rule.validate(goodAlphabet))
+        
+    }
+}
