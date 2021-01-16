33bd5318e9b41dd99a9278a60d85b0acda29a05d
Mukul Agarwal <55563106+agarmu@users.noreply.github.com>
2021-01-16 13:12:19 -0600

    make alphabet a required field to comply with requirements

--- Sources/Lindenmayer/Lindenmayer.swift
+++ Sources/Lindenmayer/Lindenmayer.swift
@@ -2,20 +2,19 @@ class LSystem<T: Hashable & Equatable, U: Collection & Equatable & Take>
     where U.Element == T, U.T == T
 {
     private let rules: [T : U];
-    private var validator: (T) -> Bool = { (_ : T) -> Bool in true };
     private let axiom : U;
     private var state : U;
-    private let alphabet: Set<T>?;
-    init(alphabet: U? = nil, validator: Optional<(T) -> Bool> = nil, axiom: U, productionRules: [ProductionRule<T,U>]){
-        self.alphabet = LSystem.initializeAlphabet(alphabet)
+    private let alphabet: Set<T>;
+    init<V: Collection>(alphabet: V, validator: Optional<(T) -> Bool> = nil, axiom: U, productionRules: [ProductionRule<T,U>]) throws where V.Element == T{
+        self.alphabet = Set(alphabet)
         self.axiom = axiom
         self.state = axiom
-        self.rules = LSystem.initializeRules(productionRules)
-        if let v = validator {
-            self.validator = v
-        } else if alphabet != nil  {
-            self.validator = { self.alphabet!.contains($0) }
+        do {
+            self.rules = try LSystem.initializeRules(productionRules, self.alphabet)
+        } catch {
+            throw error
         }
+        
     }
     func produce(generationCount: UInt = 1) -> U {
         if generationCount == 0 {
@@ -29,17 +28,16 @@ class LSystem<T: Hashable & Equatable, U: Collection & Equatable & Take>
         self.state = U.take(res)
         return produce(generationCount: generationCount-1)
     }
-    private static func initializeAlphabet(_ alphabet: U?) -> Set<T>? {
-        if let alphabet = alphabet {
-            return Set(alphabet)
-        } else {
-            return nil
-        }
-    }
-    private static func initializeRules(_ productionRules: [ProductionRule<T, U>]) -> Dictionary<T, U> {
+    private static func initializeRules(_ productionRules: [ProductionRule<T, U>], _ alphabet: Set<T>) throws -> Dictionary<T, U> {
         var rules : Dictionary<T, U> = Dictionary()
         for rule in productionRules {
-            rules[rule.predecessor] = rule.successor
+            do {
+                try rule.validate(alphabet)
+                rules[rule.predecessor] = rule.successor
+            } catch {
+                throw error
+            }
+            
         }
         return rules
     }
--- Tests/LindenmayerTests/LindenmayerTests.swift
+++ Tests/LindenmayerTests/LindenmayerTests.swift
@@ -3,18 +3,18 @@ import XCTest
 
 final class LindenmayerTests: XCTestCase {
     typealias Rules = [ProductionRule<Character, String>]
-    func SimpleL(_ inputRules: [Character:String], _ axiom: String) -> (UInt) -> String {
+    func SimpleL(_ alphabet: String, _ inputRules: [Character:String], _ axiom: String) -> (UInt) -> String {
         var rules: Rules = []
         for rule in inputRules {
             rules.append(.init(predecessor: rule.key, successor: rule.value))
         }
-        let sys = LSystem(axiom: axiom, productionRules: rules)
+        let sys = try! LSystem(alphabet: alphabet, axiom: axiom, productionRules: rules)
         return {(generations: UInt) -> String in
             sys.produce(generationCount: generations)
         }
     }
     func testIncrementalAlgae() {
-        let sys = SimpleL(["A" : "AB", "B" : "A"], "A")
+        let sys = SimpleL("AB", ["A" : "AB", "B" : "A"], "A")
         XCTAssertEqual(sys(1), "AB")
         XCTAssertEqual(sys(1), "ABA")
         XCTAssertEqual(sys(1), "ABAAB")
@@ -25,30 +25,30 @@ final class LindenmayerTests: XCTestCase {
     }
     func testAlgae() {
         //src:https://en.wikipedia.org/wiki/L-system#Example_1:_Algae
-        let sys = SimpleL(["A": "AB", "B":"A"], "A")
+        let sys = SimpleL("AB", ["A": "AB", "B":"A"], "A")
         XCTAssertEqual(sys(7), "ABAABABAABAABABAABABAABAABABAABAAB")
     }
     
     func testFractalTree() {
         //src:https://en.wikipedia.org/wiki/L-system#Example_1:_Algae
-        let sys = SimpleL(["1":"11","0":"1[0]0"], "0")
+        let sys = SimpleL("110[]", ["1":"11","0":"1[0]0"], "0")
         XCTAssertEqual(sys(3), "1111[11[1[0]0]1[0]0]11[1[0]0]1[0]0")
     }
     
     func testCantorSet() throws {
         //src:https://en.wikipedia.org/wiki/L-system#Example_3:_Cantor_set
-        let sys = SimpleL(["A":"ABA","B":"BBB"], "A")
+        let sys = SimpleL("AB", ["A":"ABA","B":"BBB"], "A")
         XCTAssertEqual(sys(3), "ABABBBABABBBBBBBBBABABBBABA")
     }
 
     func testKochCurve() throws {
         //src:https://en.wikipedia.org/wiki/L-system#Example_4:_Koch_curve
-        let sys = SimpleL(["F":"F+F-F-F+F"], "F")
+        let sys = SimpleL("F+-", ["F":"F+F-F-F+F"], "F")
         XCTAssertEqual(sys(2), "F+F-F-F+F+F+F-F-F+F-F+F-F-F+F-F+F-F-F+F+F+F-F-F+F")
     }
     func testSierpinskiTriange() {
         //src:https://en.wikipedia.org/wiki/L-system#Example_5:_Sierpinski_triangle
-        let sys = SimpleL(["F": "F-G+F+G-F", "G" : "GG"], "F-G-G")
+        let sys = SimpleL("F+-G", ["F": "F-G+F+G-F", "G" : "GG"], "F-G-G")
         XCTAssertEqual(sys(2), "F-G+F+G-F-GG+F-G+F+G-F+GG-F-G+F+G-F-GGGG-GGGG")
     }
     
