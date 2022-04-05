cef68a5e70d4f95327ed2a06470e74ff72ef85b5
Mukul Agarwal <agarmukul23@gmail.com>
2022-04-05 10:06:56 -0500

    Update Lindenmayer.swift

--- Sources/Lindenmayer/Lindenmayer.swift
+++ Sources/Lindenmayer/Lindenmayer.swift
@@ -3,12 +3,16 @@ class LSystem<T: Hashable & Equatable, U: Collection & Equatable & Take>
 {
     private let rules: [T : U];
     public let axiom : U;
-    public var state : U;
+     public var terminals: Set<T> {
+        alphabet.filter { rules[$0] == nil }
+    }
+    public var nonTerminals: Set<T> {
+        alphabet.filter { rules[$0] != nil }
+    }
     public let alphabet: Set<T>;
     init<V: Collection>(alphabet: V, validator: Optional<(T) -> Bool> = nil, axiom: U, productionRules: [ProductionRule<T,U>]) throws where V.Element == T{
         self.alphabet = Set(alphabet)
         self.axiom = axiom
-        self.state = axiom
         do {
             self.rules = try LSystem.initializeRules(productionRules, self.alphabet)
         } catch {
@@ -16,17 +20,29 @@ class LSystem<T: Hashable & Equatable, U: Collection & Equatable & Take>
         }
         
     }
-    func produce(generationCount: UInt = 1) -> U {
+     init<V: Collection>(alphabet: V, validator: Optional<(T) -> Bool> = nil, axiom: U, productionRules: [T:U]) throws where V.Element == T {
+        self.alphabet = Set(alphabet)
+        self.axiom = axiom
+        do {
+            self.rules = try LSystem.initializeDictRules(productionRules, self.alphabet)
+        } catch {
+            throw error
+        }
+    }
+    func produce(generationCount: UInt = 0) -> U {
+        let res: U
         if generationCount == 0 {
-            return self.state
+            res = axiom
+        } else {
+            var wres: [T] = []
+            let state = produce(generationCount: generationCount-1)
+            for point in state {
+                wres += self.rules[point] ?? U.take([point])
+
+            }
+            res = U.take(wres)
         }
-        var res: [T] = []
-        for point in self.state {
-            res += self.rules[point] ?? U.take([point])
-            
-        }
-        self.state = U.take(res)
-        return produce(generationCount: generationCount-1)
+        return res
     }
     private static func initializeRules(_ productionRules: [ProductionRule<T, U>], _ alphabet: Set<T>) throws -> Dictionary<T, U> {
         var rules : Dictionary<T, U> = Dictionary()
@@ -38,6 +54,13 @@ class LSystem<T: Hashable & Equatable, U: Collection & Equatable & Take>
                 throw error
             }
             
+        }
+        return rules
+    }
+    private static func initializeDictRules(_ rules: [T:U], _ alphabet: Set<T>) throws -> [T:U] {
+        for (k, v) in rules {
+            let rule = ProductionRule(k, v)
+            try rule.validate(alphabet)
         }
         return rules
     }
